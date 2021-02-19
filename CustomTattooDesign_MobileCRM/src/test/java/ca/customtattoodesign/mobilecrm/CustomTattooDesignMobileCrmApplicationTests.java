72e336291fd7e5ad1ef2ab513501d993aaee3d37
fernando <fedelef@sheridancollege.ca>
2021-02-19 13:33:22 -0500

    initial push

---
+++ CustomTattooDesign_MobileCRM/src/test/java/ca/customtattoodesign/mobilecrm/CustomTattooDesignMobileCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.customtattoodesign.mobilecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomTattooDesignMobileCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
