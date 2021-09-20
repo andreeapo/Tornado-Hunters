27f305da2ca36970e2e04799a7ac4a7e99df6a79
krutikov <krutikov@sheridancollege.ca>
2021-09-20 17:36:59 -0400

    Corrected Outdated JavaDocs in Conversation Service

--- CustomTattooDesign_MobileCRM/src/main/java/ca/customtattoodesign/mobilecrm/services/ConversationService.java
+++ CustomTattooDesign_MobileCRM/src/main/java/ca/customtattoodesign/mobilecrm/services/ConversationService.java
@@ -15,7 +15,7 @@ import ca.customtattoodesign.mobilecrm.beans.Message;
 import ca.customtattoodesign.mobilecrm.dao.TornadoHuntersDao;
 
 /**
- * The {@code ConversationService} class is used to fetch various types of jobs using a Data Access Object, it
+ * The {@code ConversationService} class is used to send and fetch messages using a Data Access Object, it
  * 		acts as a middle-man between the RESTful API and the Database.
  * 
  * @author Roman Krutikov
@@ -32,7 +32,8 @@ public class ConversationService {
 	/**
 	 * Fetches all messages for a specified job.
 	 * 
-	 * @param jobId the ID of the job for which we want to fetch messages
+	 * @param convoLogin a ConversationLog object which should contain a value for the
+	 * 		jobId (the ID of the job to which to send the message to)  
 	 * @return A list of messages that are associated with the job id
 	 */
 	public List<Message> fetchJobMessages(ConversationLogin convoLogin){
@@ -72,7 +73,8 @@ public class ConversationService {
 	/**
 	 * Fetches all messages for a specified job that have not yet been read by the other party.
 	 * 
-	 * @param jobId the ID of the job for which we want to fetch messages
+	 * @param convoLogin a ConversationLog object which should contain a value for the
+	 * 		jobId (the ID of the job to which to send the message to)  
 	 * @return A list of unread messages that are associated with the job id
 	 */
 	public List<Message> fetchUnreadJobMessages(ConversationLogin convoLogin){
@@ -112,9 +114,10 @@ public class ConversationService {
 	/**
 	 * Sends a String message to a job conversation and returns if the message was sent successfully.
 	 * 
-	 * @param jobId the ID of the job to which to send the message to
-	 * @param stringMessage the text of the message that is being sent
-	 * @param sessionToken a sessionToken to verify if the artist or the customer are sending the message
+	 * @param convoLogin a ConversationLog object which should contain values for 
+	 * 		jobId (the ID of the job to which to send the message to),  
+	 * 		body (the text of the message that is being sent) and
+	 *      sessionToken (a sessionToken to verify if the artist or the customer are sending the message)
 	 * @return {@code true} if the String message was sent successfully<br>
 	 *	       {@code false} if sending the String message failed
 	 */
@@ -124,7 +127,8 @@ public class ConversationService {
 		if (jobService.isJobIdValid(convoLogin.getJobId())) {
 			if (this.isStringMessageValid(convoLogin.getBody())) {
 				try {
-					messageSentSuccessfully = TornadoHuntersDao.getInstance().sendStringMessage(convoLogin.getJobId(), convoLogin.getBody(), convoLogin.getSessionToken());
+					messageSentSuccessfully = TornadoHuntersDao.getInstance().sendStringMessage(convoLogin.getJobId(), 
+							convoLogin.getBody(), convoLogin.getSessionToken());
 				}
 				catch (SQLException e) {
 					if (e.getMessage().contains("ERROR: Job Does Not Exist")) {
